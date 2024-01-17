940d0013670c27e75f1efda3e807cacd0aa79fdf
Sunil Kumar Kandakatla <sunilkumarkandakatla@gmail.com>
2024-01-17 18:01:28 +0530

    Array - Contains Common Item

---
+++ src/main/java/com/sunil/kumar/kandakatla/array/ContainsCommonItem.java
@@ -0,0 +1,45 @@
+package com.sunil.kumar.kandakatla.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsCommonItem {
+
+	private static boolean containsCommonItem1(String[] arr1, String[] arr2) {
+		for (int i = 0; i < arr1.length; i++) {
+			String string1 = arr1[i];
+			for (int j = 0; j < arr2.length; j++) {
+				String string2 = arr2[j];
+				if (string1.equals(string2)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private static boolean containsCommonItem2(String[] arr1, String[] arr2) {
+		Map<String, Boolean> map = new HashMap<>();
+		for (int j = 0; j < arr2.length; j++) {
+			String string2 = arr2[j];
+			map.put(string2, true);
+		}
+
+		for (int i = 0; i < arr1.length; i++) {
+			String string1 = arr1[i];
+			if (map.get(string1)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		String[] arr1 = { "a", "b", "c", "x" };
+		String[] arr2 = new String[] { "z", "y", "a" };
+
+		System.out.println(containsCommonItem1(arr1, arr2));
+		System.out.println(containsCommonItem2(arr1, arr2));
+	}
+
+}
--- src/main/java/com/sunil/kumar/kandakatla/array/HasPairWithSum.java
+++ src/main/java/com/sunil/kumar/kandakatla/array/HasPairWithSum.java
@@ -1,4 +1,4 @@
-package com.sunil.kumar.kandakatla;
+package com.sunil.kumar.kandakatla.array;
 
 import java.util.HashSet;
 import java.util.Set;
