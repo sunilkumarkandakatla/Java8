33fb15a877aa2177c53b5ab19fa620d4a5f2c8fc
Sunil Kumar Kandakatla <sunilkumarkandakatla@gmail.com>
2024-01-17 18:08:56 +0530

    Array - Log All Pairs Of Array

---
+++ src/main/java/com/sunil/kumar/kandakatla/array/LogAllPairsOfArray.java
@@ -0,0 +1,21 @@
+package com.sunil.kumar.kandakatla.array;
+
+public class LogAllPairsOfArray {
+
+	private static void logAllPairsOfArray(char[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				System.out.println("[" + arr[i] + "," + arr[j] + "]");
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		// Log all pairs of array
+
+		char[] arr = { 'a', 'b', 'c', 'd', 'e' };
+
+		logAllPairsOfArray(arr);
+	}
+
+}
